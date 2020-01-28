19cf8947de4340831ad68d4e444bcf21efdb969e
Sandeep Bagchi <sandeep.bagchi@gmail.com>
2020-01-28 12:41:33 +0530

    List keys in js object.

--- wasm_go/main.go
+++ wasm_go/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/thebagchi/learn-wasm/wasm_go/utils"
+	"syscall/js"
 )
 
 func main() {
+	{
+		keys := utils.Keys(js.Global())
+		fmt.Println("Len: ", len(keys))
+		if len(keys) > 0 {
+			for _, key := range keys {
+				fmt.Println(key)
+			}
+		}
+	}
 	fmt.Println("Hello World")
 }
